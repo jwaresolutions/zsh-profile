67e7b23d35400611d582226d2c9a4d4e46331d0b
Your Name <you@example.com>
2023-05-16 18:47:45 -0700

    updates

--- setup2.sh
+++ setup2.sh
@@ -9,7 +9,7 @@ pip install gdown
 #cd nerd-fonts
 #./install.sh
 #cd ~ && rm -rf nerd-fonts
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k/
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 git clone https://github.com/jwaresolutions/zsh-profile.git ~/zsh-profile/
