0ad0fd46a7d422718f3ffe725b7981d7fdcc34b1
Your Name <you@example.com>
2023-05-16 17:15:32 -0700

    updates

--- setup.sh
+++ setup.sh
@@ -5,7 +5,10 @@ sudo apt update && sudo apt upgrade -y && sudo apt install git python3 python3-p
 git clone https://github.com/ryanoasis/nerd-fonts.git
 cd nerd-fonts
 ./install.sh
+cd ~
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+git clonehttps://github.com/jwaresolutions/zsh-profile.git
+cd zsh-profile
 cp .p10k.zsh ~/
 cp .zshrc ~/
-
+source ~/.zshrc
