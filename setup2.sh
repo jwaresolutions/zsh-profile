b59e685c0d333031fcac743aa7714b78ff0686d2
jwaresolutions <jmalone@jwaresolutions.com>
2023-05-18 23:48:02 -0700

    Update setup2.sh

--- setup2.sh
+++ setup2.sh
@@ -1,14 +1,9 @@
-#!bin/bash
+#!bin/zsh
 
 # install some packages
 sudo apt install git python3 python3-pip python-is-python3 python-is-python3 gdown -y
 pip install gdown
 
-# install powerlevel10k and dependencies
-#git clone https://github.com/ryanoasis/nerd-fonts.git
-#cd nerd-fonts
-#./install.sh
-#cd ~ && rm -rf nerd-fonts
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k/
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
