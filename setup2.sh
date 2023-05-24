02068e5e9f8d5107f7261dd7c2b85292dccb90c3
jwaresolutions <jmalone@jwaresolutions.com>
2023-05-23 20:31:03 -0700

    Update setup2.sh

--- setup2.sh
+++ setup2.sh
@@ -5,9 +5,9 @@ source ~/.zshrc
 sudo apt install git python3 python3-pip python-is-python3 python-is-python3 -y
 pip install gdown
 
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k/
-git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k/
+git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions/
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting/
 git clone https://github.com/jwaresolutions/zsh-profile.git ~/zsh-profile/
 
 cp ~/zsh-profile/.p10k.zsh ~/
