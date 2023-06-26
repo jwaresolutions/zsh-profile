6adbb746a9163e6ecafb8c1a5f0ddf9e9559ef22
jwaresolutions <jmalone@jwaresolutions.com>
2023-06-25 17:55:14 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,8 +3,5 @@
 # install zxh
 sudo apt update && sudo apt upgrade -y && sudo apt -qq install zsh -y
 # install oh-my-zsh
-#sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh
-sed -i.tmp 's:env zsh::g' install.sh
-sed -i.tmp 's:chsh -s .*$::g' install.sh
-sh install.sh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
