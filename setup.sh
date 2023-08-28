c075f6e1e0714b4972c84ec2a064ab9abe7488d1
jwaresolutions <jmalone@jwaresolutions.com>
2023-08-28 08:40:34 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,26 @@
 #!bin/bash
 
-# install zxh
-sudo apt update && sudo apt upgrade -y && sudo apt -qq install zsh -y
-# install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# install zsh
+sudo apt update 
+DEBIAN_FRONTEND='noninteractive' apt-get -y -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold' upgrade
+DEBIAN_FRONTEND='noninteractive' apt-get -y -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold' dist-upgrade
 
+# install some packages
+DEBIAN_FRONTEND='noninteractive' apt install -y -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold' git python3 python3-pip python-is-python3 python-is-python3 zsh
+pip install gdown -y
+apt-get autoremove -y
+apt-get clean
+apt-get autoclean
+
+# install oh-my-zsh and theme/plugins/fonts
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k/
+git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions/
+git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting/
+git clone https://github.com/jwaresolutions/zsh-profile.git ~/zsh-profile/
+
+cp ~/zsh-profile/.p10k.zsh ~/
+cp ~/zsh-profile/.zshrc ~/
+# cd ~ && rm -rf ~/zsh-profile
+zsh
+source ~/.zshrc
