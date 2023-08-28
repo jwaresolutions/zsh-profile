0f02089db3005ec86276cbdeb007b2e80a8c562b
jwaresolutions <jmalone@jwaresolutions.com>
2023-08-28 08:31:06 -0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,25 +1,26 @@
-still failing check https://www.reddit.com/r/zsh/comments/riokz2/unattended_install/
-later
 #!bin/bash
 
 # install zsh
-DEBIAN_FRONTEND=noninteractive
-sudo apt update && sudo apt upgrade -y
-curl -L git.io/antigen > antigen.zsh #from https://github.com/zsh-users/antigen
-# install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sed 's:env zsh::g' | sed 's:chsh -s .*$::g')"
+sudo apt update 
+DEBIAN_FRONTEND='noninteractive' apt-get -y -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold' upgrade
+DEBIAN_FRONTEND='noninteractive' apt-get -y -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold' dist-upgrade
 
 # install some packages
-sudo apt install git python3 python3-pip python-is-python3 python-is-python3 -y
+DEBIAN_FRONTEND='noninteractive' apt install -y -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold' git python3 python3-pip python-is-python3 python-is-python3 zsh
 pip install gdown -y
+apt-get autoremove -y
+apt-get clean
+apt-get autoclean
 
+# install oh-my-zsh and theme/plugins/fonts
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k/
-git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions/
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting/
+git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions/
+git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting/
 git clone https://github.com/jwaresolutions/zsh-profile.git ~/zsh-profile/
 
 cp ~/zsh-profile/.p10k.zsh ~/
 cp ~/zsh-profile/.zshrc ~/
 # cd ~ && rm -rf ~/zsh-profile
-/bin/zsh /home/main/.zshrc
+zsh
 source ~/.zshrc
