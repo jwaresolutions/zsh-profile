39852e6b318dc7f5d34ed108092cc800c4c157cf
jwaresolutions <jmalone@jwaresolutions.com>
2023-10-24 11:12:56 -0700

    Update anaconda.zsh

--- bin/anaconda.zsh
+++ bin/anaconda.zsh
@@ -1,4 +1,4 @@
-#!bin/zsh
+#!/bin/zsh
 
 # the wget command from below was the latest when this was created. dont be lazy and get the latest version. copy the name of the latest version of Anaconda3.*Linux-x86_64.sh from https://repo.anaconda.com/archive/
 
